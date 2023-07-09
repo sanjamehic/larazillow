b6e477237d16227ee0579114e3efb0e0497f0677
Sanja Mehic <ms.sanjamehic@gmail.com>
2023-07-10 00:47:58 +0200

    Master Laravel, Vue 3 & Inertia Full Stack 2023 course first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <div>Index</div>
+    <div>
+        The message is {{ message }}
+    </div>
+
+</template>
+<script setup>
+import MainLayout from '../../Layouts/MainLayout.vue';
+defineProps(['message'])
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,9 @@
+<template>
+
+    <div>Show</div>
+
+
+</template>
+<script setup>
+import MainLayout from '../../Layouts/MainLayout.vue';
+</script>
