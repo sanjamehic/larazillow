7d30e8ca1243ae15d307bec1366ae2f099c8aaba
Sanja Mehic <ms.sanjamehic@gmail.com>
2023-07-12 10:41:25 +0200

    Displaying flash messages

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,15 +2,30 @@
     <Link href="/">Main Page</Link>
     <Link href="/hello">Show Page</Link>
     <!-- <div>The page with time {{ timer }}</div> -->
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
     <slot></slot>
 
 </template>
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { computed } from 'vue';
+import { Link, usePage } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
-const timer = ref(0)
-setInterval(
-    () => timer.value++, 1000
+//page.props.value.flash.success
+const page = usePage()
+const flashSuccess = computed(
+    ()=>page.props.flash.success
 )
+// const timer = ref(0)
+// setInterval(
+//     () => timer.value++, 1000
+// )
 </script>
+
+<style>
+.success {
+    background-color: greenyellow;
+}
+</style>
