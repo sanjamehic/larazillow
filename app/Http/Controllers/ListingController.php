3ffe6d867b30baa149fff391608338219681028d
Sanja Mehic <ms.sanjamehic@gmail.com>
2023-07-13 11:39:21 +0200

    Deleting data

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -103,8 +103,11 @@ class ListingController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Listing $listing)
     {
-        //
+        $listing->delete();
+
+        return redirect()->back()
+            ->with('success', 'Listing deleted');
     }
 }
