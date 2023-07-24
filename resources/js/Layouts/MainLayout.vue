59d40ab78a3fd57992b917d22309c211a2ecec8c
Sanja Mehic <ms.sanjamehic@gmail.com>
2023-07-24 23:14:59 +0200

    Auth controller, routes, form

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -16,7 +16,7 @@
         </div>
     </header>
 
-    <main class="container mx-auto p-4">
+    <main class="container mx-auto p-4 w-full">
         <div v-if="flashSuccess" class="mb-4 borer rounded-md shadow-sm border-green-500 dark:bg-green-800 bg-green-50 p-2">
             {{ flashSuccess }}
         </div>
