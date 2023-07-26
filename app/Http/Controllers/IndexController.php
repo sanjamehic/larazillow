abb954b362304102fcb9f67852e82859bbc83c71
Sanja Mehic <ms.sanjamehic@gmail.com>
2023-07-26 23:43:16 +0200

    Login and Authentication process

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,21 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index()
     {
+
+        dd(Auth::user());
+
         return inertia(
             'Index/Index',
             [
                 'message' => 'Hello from Laravel!'
             ]
+
         );
+
     }
 
     public function show()
     {
+
         return inertia('Index/Show');
     }
 }
