129bb3ecc86ccc81a04b732327575e825c5ad9fa
Sanja Mehic <ms.sanjamehic@gmail.com>
2023-08-19 23:37:30 +0200

    Model Policy and Guest Users

--- app/Policies/ListingPolicy.php
+++ app/Policies/ListingPolicy.php
@@ -11,15 +11,16 @@ class ListingPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(?User $user): bool
     {
-        //
+        //? means that the parameter is optional
+        return true;
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Listing $listing): bool
+    public function view(?sUser $user, Listing $listing): bool
     {
         return true;
     }
