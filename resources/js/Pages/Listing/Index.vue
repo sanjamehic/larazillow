dac5b611ffc44e7d18cac612fab0d2e06a835a03
Sanja Mehic <ms.sanjamehic@gmail.com>
2023-08-21 00:11:59 +0200

    Filtering JS Logic

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <Filters />
+    <Filters :filters="filters"/>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />    </div>
     <div v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4">
@@ -13,5 +13,6 @@ import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
 import Filters from '@/Pages/Listing/Index/Filters.vue';
     defineProps({
         listings: Object,
+        filters: Object,
     })
  </script>
