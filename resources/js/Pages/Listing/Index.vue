077ecd3f69bcc3b45bb77f26016d8d0df6560e3d
Sanja Mehic <ms.sanjamehic@gmail.com>
2023-08-20 23:26:33 +0200

    Filtering Form Markup

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,7 +1,8 @@
 <template>
+    <Filters />
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />    </div>
-    <div v-if="listings.data.length" class="w-full flex justify-center mt-8 mb-8">
+    <div v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4">
         <Pagination :links="listings.links"/>
     </div>
 </template>
@@ -9,6 +10,7 @@
 <script setup>
 import Pagination from '@/Components/UI/Pagination.vue';
 import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
+import Filters from '@/Pages/Listing/Index/Filters.vue';
     defineProps({
         listings: Object,
     })
