8f5045c4839515469c644950c4ab062b44a9c388
Sanja Mehic <ms.sanjamehic@gmail.com>
2023-09-19 11:46:45 +0200

    Debounce Function

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -15,18 +15,18 @@
 <script setup>
     import { reactive, watch } from 'vue';
     import { Inertia } from '@Inertiajs/inertia';
+    import {debounce} from 'lodash'
 
     const filterForm = reactive({
         deleted: false,
     })
     //can be only reactive ref or computed
     watch(
-        filterForm, () => Inertia.get(
+        filterForm, debounce(()=> Inertia.get(
             route('realtor.listing.index'),
             filterForm,
             {preserveScroll:true, preserveState:true}
-
-        )
-        )
+        ), 1000)
+    )
     //loses reactivity when passed from reactive() to another function so it must be wrapped in getter function () =>
 </script>
